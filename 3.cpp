56a6b7ac4d8ba330520282a6e6ac26aff51cc4ca
Agmongui <mary.galitsina@gmail.com>
2026-09-22 22:02:21 +0500

    Создала функцию printArray и int& getElement(SafeArray& arr, int index), которая выполняет проверку индекса

--- 3.cpp
+++ 3.cpp
@@ -17,6 +17,28 @@ SafeArray createArray(int size)
     return arr;
 }
 
+void printSafe(const SafeArray& arr)
+{
+    for (int index{}; index < arr.size; index++)
+    {
+        cout << arr.data[index] << ' ';
+    }
+    cout << '\n';
+}
+
+int& getElement(SafeArray& arr, int index)
+{
+    if (index < 0 || index >= arr.size)
+    {
+        cout << "Индекс вышел за границы";
+        static int a = 0;
+        return a;
+    }
+
+    return arr.data[index];  
+}
+
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
@@ -27,6 +49,16 @@ int main()
     cin >> size;
 
     SafeArray arr = createArray(size);
+
+    cout << "Введите элементы массива:\n";
+
+    for (int index{}; index < arr.size; index++)
+    {
+        cin >> arr.data[index];
+    }
+    cout << "Массив: ";
+    printSafe(arr);
+
 
     delete[] arr.data;
     return 0;
