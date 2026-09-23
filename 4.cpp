5d136aade4e8f3adaa9e72f6269eb75d4598e922
Agmongui <mary.galitsina@gmail.com>
2026-09-23 12:32:46 +0500

    Создала функцию красивого вывода с параметарми по умолчанию void printMatrix

--- 4.cpp
+++ 4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <string>
 
 using namespace std;
 
@@ -15,13 +16,59 @@ int** allocateMatrix(int rows, int cols)
 
 void fillMatrix(int** matrix, int rows, int cols)
 {
-    cout << "Введите оценки: ";
+    cout << "Введите оценки:\n";
     for (int i{}; i < rows; ++i)
     {
         for (int j = 0; j < cols; j++)
         {
-            cout << "Студент " << i + 1 << "его оценка " << j + 1 << ": ";
-            cin >> matrix[i][j];
+            cout << "Студент " << i + 1 << " его оценка " << j + 1 << ": ";
+            cin >> matrix[i][j]; 
+        }
+    }
+}
+
+void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, string title = "Matrix")
+{
+    cout << title << '\n';
+
+    const int cellWidth = 3;
+
+    if (showBorders)
+    {
+
+        const int borderLen = 1 + cols * cellWidth;
+
+        for (int symbol{}; symbol < borderLen; symbol++)
+            cout << '*';
+        cout << '\n';
+
+        for (int row{}; row < rows; row++)
+        {
+            cout << '*';
+            for (int col{}; col < cols; col++)
+            {
+                cout << ' ' << matrix[row][col];
+
+                int digits = to_string(matrix[row][col]).size();
+                for (int s{}; s < cellWidth - digits - 1; s++)
+                    cout << ' ';
+            }
+            cout << "*\n";
+        }
+
+        for (int symbol{}; symbol < borderLen; symbol++)
+            cout << '*';
+        cout << '\n';
+    }
+    else
+    {
+        for (int row{}; row < rows; row++)
+        {
+            for (int col{}; col < cols; col++)
+            {
+                cout << matrix[row][col] << ' ';
+            }
+            cout << '\n';
         }
     }
 }
@@ -39,8 +86,18 @@ int main()
 
     cout << "Кол-во оценок: ";
     cin >> cols;
-    
+
     int** matrix {allocateMatrix(rows, cols)};
     fillMatrix(matrix, rows, cols);
+
+    printMatrix(matrix, rows, cols);
+
+    printMatrix(matrix, rows, cols, true, "\nТаблица оценок");
+
+    printMatrix(matrix, rows, cols, false, "\nОценки без рамки");
+
+    matrix = nullptr;
+
+    return 0;
 
 }
